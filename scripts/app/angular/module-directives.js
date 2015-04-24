cea020102c3e575e24764a9b79b58e04d369080a
Christos Koumenides <chriskmnds@gmail.com>
2015-04-24 12:34:56 +0300

    general updates - moved templates to templates dir

--- scripts/app/angular/module-directives.js
+++ scripts/app/angular/module-directives.js
@@ -28,14 +28,14 @@ var app = angular.module('app.main', ['app.init', 'CustomFilter', 'ngAnimate'])
 		// Set the meta values of the init module
 		meta.setName('City Stats Cyprus');
 		meta.setAuthor('Christos Koumenides');
-		meta.setDescription('Open data visualisation built in Angular and D3.js. Explores 2011 statistics about xxx cities and villages in Cyprus. Allows grouping of cities/villages for deeper exploration and comparison. Cities/villages can be grouped together for aggregated results, hence deeper exploration and comparison. Data sourced from the Cyprus Statistical Agency. Datasets used: xxx, xxx, and xxx.');
+		meta.setDescription('Open data visualisation built in Angular and D3.js. Explores 2011 statistics about xxx cities and villages in Cyprus. Allows grouping of cities/villages for deeper exploration and comparison. Data sourced from the Cyprus Statistical Agency. Datasets used: xxx, xxx, and xxx.');
 	});
 
 app.directive("utilsSelectWidget", function () {
 	return {
 		restrict: 'E',
 		replace: true,
-		templateUrl: 'utilsTooltip.html'
+		templateUrl: 'templates/utilsTooltip.html'
 	};
 });
 
@@ -43,7 +43,7 @@ app.directive("utilsNodeWidget", function () {
 	return {
 		restrict: 'E',
 		replace: true,
-		templateUrl: 'nodeTooltip.html'
+		templateUrl: 'templates/nodeTooltip.html'
 	};
 });
 
@@ -51,6 +51,6 @@ app.directive("statsDetailsWidget", function () {
 	return {
 		restrict: 'E',
 		replace: true,
-		templateUrl: 'statsDetails.html'
+		templateUrl: 'templates/statsDetails.html'
 	};
 });
